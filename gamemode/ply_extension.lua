52b26d483c2480af196112e432ecd42c00d32e91
hurrysworld <hurrysworld@817d7f9e-d16d-11de-9f39-755c91dee682>
2009-11-18 21:20:08 +0000

    Eventual fixes

--- gamemode/ply_extension.lua
+++ gamemode/ply_extension.lua
@@ -8,6 +8,10 @@ function meta:ForceFlashlight( switchOn )
 	self._ForceFlashlight = false
 end
 
+function meta:IsSpectating()
+	return ( self:Team() == TEAM_SPECTATOR and self:GetObserverMode() > OBS_MODE_NONE )
+end
+
 function meta:IsObserver()
-	return ( self:Team() == TEAM_SPECTATOR && self:GetObserverMode() > OBS_MODE_NONE );
+	return ( self:GetObserverMode() > OBS_MODE_NONE )
 end
